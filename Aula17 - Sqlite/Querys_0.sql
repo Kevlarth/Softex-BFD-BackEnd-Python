ba389d68e26f6db2c71fe5c7397ceaf6ebe10157
Luiz H G Silva <luiizhenriq@outlook.com>
2025-10-20 01:34:44 +0000

    Introdução a SQL

---
+++ Aula17 - Sqlite/Querys_0.sql
@@ -0,0 +1,10 @@
+-- https://inloop.github.io/sqlite-viewer - visualizar arquivos .sqlite
+
+-- Consultar todas as tabelas no banco de dados
+.tables
+
+-- Mostrar a estrutura da tabela Aluno
+.schema Aluno
+
+-- Selecionar todos os registros da tabela Aluno
+SELECT * FROM Aluno;
